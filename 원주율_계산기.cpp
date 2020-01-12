6416d2029893c5637380cea409cfaf1cb6c29bfc
xcellentbird <xcellentbird@gmail.com>
2020-01-13 02:17:49 +0900

    초기 커밋.

---
+++ 원주율_계산기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+constexpr auto pi = 3.141592;
+using namespace std;
+
+int main(void)
+{
+	int radius;
+	cout << "반지름을 입력하시오: ";
+	cin >> radius;
+
+	double area = 2*pi*pow(radius,2);
+	cout << "원의 넓이는 " << area << "입니다.";
+
+	return 0;
+}
