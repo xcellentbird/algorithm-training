158af4c982d29627022e3fbdc587be523e7e723e
xcellentbird <59414764+xcellentbird@users.noreply.github.com>
2020-12-22 01:15:10 +0900

    Update 선택 정렬.py

--- 정렬/선택 정렬.py
+++ 정렬/선택 정렬.py
@@ -8,14 +8,26 @@ import random
 # timeit 모듈에서 default_timer 클래스를 timer이름으로 불러온다
 from timeit import default_timer as timer
 
-# random.sample(범위, 개수)
-x = random.sample(range(10000), 100)
-
 def selection_sort(A):
     for last in range(len(A)-1, 0, -1): # len(A)-1부터 1까지 -1해가며
         largest = 0 # 가장 큰 원소의 index
         for i in range(1, last+1): # 1부터 last까지
-            if A[i] > A[largest]:
-                largest=i
-        x[largest],x[last] = x[last],[largest] # switch data
+            if A[i] > A[largest]: #현재 원소가 최대 원소보다 크면
+                largest=i # 해당 원소의 index를 최대 원소 index로
+        x[largest],x[last] = x[last],x[largest] # 나중 원소와 가장 큰 원소를 맞바꾼다
+
+def test(A):
+    for i in range(1, len(A)):
+        if A[i-1] > A[i]:
+            return False
+    return True
+
+# random.sample(범위, 개수)
+x = random.sample(range(10000), 10)
+print(x)
+start = timer() # 현재 시간을 불러와 저장
+selection_sort(x) # x 행렬 선택 정렬
+print(timer() - start)
+print(x)
+print(test(x))
 
