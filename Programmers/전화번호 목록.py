db50f618a36f4d3fac2dcca3c1d4207c33807178
xcellentbird <59414764+xcellentbird@users.noreply.github.com>
2021-02-05 03:49:03 +0900

    Update 전화번호 목록.py

--- Programmers/전화번호 목록.py
+++ Programmers/전화번호 목록.py
@@ -1,3 +1,16 @@
+# 최적화에 많이 신경을 쓰려한 티가 많이 보인다.
+# 처음에는 O(N^3) 시간 복잡도를 가진 함수였지만, N^2로 최적화하였다.
+# 다른 사람 풀이에서는 startwith()함수를 사용하였다.
+"""
+def solution(phoneBook):
+    phoneBook = sorted(phoneBook)
+
+    for p1, p2 in zip(phoneBook, phoneBook[1:]):
+        if p2.startswith(p1):
+            return False
+    return True
+"""
+
 def solution(phone_book):
     answer = True
     phone_book = list(map(list, phone_book))
