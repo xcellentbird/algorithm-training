469a9e8d57c2966c73b1c091a5943ddb41505add
xcellentbird <59414764+xcellentbird@users.noreply.github.com>
2021-02-21 23:57:20 +0900

    100.0 / 100.0

---
+++ Programmers/주식 가격.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer(prices.size());
+    for(int i = 0; i < prices.size()-1; i++)
+        for(int j = i+1; j < prices.size(); j++){
+            answer[i]++;
+            if(prices[j] < prices[i]) break;
+        }
+    
+    return answer;
+}
