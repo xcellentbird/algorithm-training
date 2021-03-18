51cb30a5756f54108b7587164b89f871861fec79
xcellentbird <59414764+xcellentbird@users.noreply.github.com>
2021-03-19 05:54:54 +0900

    Create 여행경로.py

---
+++ Programmers/여행경로.py
@@ -0,0 +1,17 @@
+def dfs(tickets, track):
+    if not tickets:
+        return track
+    
+    ans = []
+    for i, t in enumerate(tickets):
+        if t[0] == track[-1]:
+            ti = tickets.copy()
+            tr = track.copy()
+            tr.append(ti.pop(i)[1])
+            ans = dfs(ti, tr)
+            if ans:
+                return ans
+
+def solution(tickets):
+    tickets.sort(key=lambda x: x[1])
+    return dfs(tickets,['ICN'])
