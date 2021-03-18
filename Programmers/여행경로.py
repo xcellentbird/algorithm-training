fc61d83621d036ddda4da6c3e1d1e4fb80aae28e
xcellentbird <59414764+xcellentbird@users.noreply.github.com>
2021-03-19 06:38:20 +0900

    Update 여행경로.py

--- Programmers/여행경로.py
+++ Programmers/여행경로.py
@@ -2,15 +2,16 @@ def dfs(tickets, track):
     if not tickets:
         return track
     
-    ans = []
+    #ans = []
     for i, t in enumerate(tickets):
         if t[0] == track[-1]:
             ti = tickets.copy()
             tr = track.copy()
             tr.append(ti.pop(i)[1])
-            ans = dfs(ti, tr)
+            ans = dfs(ti, tr) #ans += dfs(ti, tr)
             if ans:
-                return ans
+                return ans    
+    #return ans
 
 def solution(tickets):
     tickets.sort(key=lambda x: x[1])
