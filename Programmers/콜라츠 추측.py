62d9c51066c8abe6fa3ae7ffc319ffc73f510166
xcellentbird <59414764+xcellentbird@users.noreply.github.com>
2021-03-29 23:58:53 +0900

    Update 콜라츠 추측.py

--- Programmers/콜라츠 추측.py
+++ Programmers/콜라츠 추측.py
@@ -1,13 +1,13 @@
 def solution(num):
     answer = 0
     while answer < 500:
+        if num == 1:
+            break
         answer += 1
         if not num%2:
             num = num // 2
         else:
             num = num * 3 + 1
-        if num == 1:
-            break
     if answer == 500:
         return -1
     return answer
