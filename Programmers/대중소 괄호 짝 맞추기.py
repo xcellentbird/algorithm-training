b8235d9c592dd9f634381a45d481e6473b43d3fb
Michael Kim <59414764+xcellentbird@users.noreply.github.com>
2021-04-21 15:12:41 +0900

    Update 대중소 괄호 짝 맞추기.py

--- Programmers/대중소 괄호 짝 맞추기.py
+++ Programmers/대중소 괄호 짝 맞추기.py
@@ -1,18 +1,14 @@
 def solution(s):
-    answer = False
-    garo = {'(':[0,1], ')':[0,-1], '{':[1,1], '}':[1,-1], '[':[2,1], ']':[2,-1]}
-    score = [0, 0, 0]
-    now = 0
-    for i, ss in enumerate(s):
-        score[garo[ss][0]] += garo[ss][1]
-        if garo[ss][1] == -1:
-            if score[garo[ss][0]] < 0:
-                return False
-            elif not score[0] and not score[1] and score[2]:
-                if s[now:i+1] != s[now:i+1:-1]:
-                    print(s[now:i+1], s[now:i+1:-1])
-                    return False
-                now = i+1
-    if not sum(score):
-        return True
-    return answer
+    stack = []
+    d = {']':'[', '}':'{',')':'('}
+    for ss in s:
+        stack.append(ss)
+        while len(stack) > 1:
+            if stack[-2] == d.get(stack[-1]):
+                stack.pop()
+                stack.pop()
+            else:
+                break
+    if stack:
+        return False
+    return True
